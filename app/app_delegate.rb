6d30d1cf6d94ed6e7329e4dffb360db60e5cc19d
Eleni Chourouzidou <eleni@webspirit.gr>
2014-03-31 00:32:25 +0300

    First impressions: I love it.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,11 +1,18 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisible
-    @blue_view = UIView.alloc.
-    # shieldsController = ShieldsController.alloc.initWithStyle(UITableViewStylePlain)
 
-    # @window.rootViewController = shieldsController
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window.makeKeyAndVisible
+
+    @red_view = UIView.alloc.initWithFrame(CGRect.new([10, 10], [150, 150]))
+    @red_view.backgroundColor = UIColor.redColor
+    @window.addSubview(@red_view)
+
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@blue_view)
+
+    puts "Test console."
     true
   end
 end
